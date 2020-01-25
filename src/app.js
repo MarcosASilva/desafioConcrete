078e80d1c101930a2d9eba6fc567e5fd5dd237a1
marcos <Marcosvlk15@gmail.com>
2020-01-25 17:58:50 -0300

    first

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require("express")
+
+const app = express()
+
+app.post('/',(req,res) => {
+
+})
+app.listen(3000, () => {
+    console.log("Server running in port 3000");
+    
+})
