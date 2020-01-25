c2d99fba0ef30fa8b53672c877c47ebddde8ebff
marcos <Marcosvlk15@gmail.com>
2020-01-25 18:30:28 -0300

    Adicionada Rota

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ const express = require("express")
 
 const app = express()
 
-app.post('/',(req,res) => {
-
+app.get('/',(req,res) => {
+res.send("oi")
 })
 app.listen(process.env.port || 3000, () => {
     console.log("Server running in port 3000");
